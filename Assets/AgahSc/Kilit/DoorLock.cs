cbfcdc06d4ddc9896f5581e0560e48687c9d6066
Agah <agahgames260@gmail.com>
2023-12-09 21:12:57 +0300

    LayerAssteUpdate!!! +small updates

--- Assets/AgahSc/Kilit/DoorLock.cs
+++ Assets/AgahSc/Kilit/DoorLock.cs
@@ -4,13 +4,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoorLock : MonoBehaviour,ILock
+public class DoorLock : MonoBehaviour, ILock
 {
-    [Header("This component needs to be used with a parent object")]
-
     [SerializeField] Transform doorA_targetTransform;
     [SerializeField] Transform doorB_targetTransform;
-    
+    Vector3 doorA_startPos;
+    Vector3 doorB_startPos;
+
     [SerializeField] GameObject doorA;
     [Tooltip("No need to use 2 doors but anyway")]
     [SerializeField] GameObject doorB;
@@ -21,6 +21,11 @@ public class DoorLock : MonoBehaviour,ILock
     [SerializeField] DoorLockType type;
 
     public event Action OnDoorUnlocked;
+    private void Start()
+    {
+        doorA_startPos = doorA.transform.position;
+        doorB_startPos = doorB.transform.position;
+    }
     public void GetUnlocked()
     {
         Debug.Log("DoorLock Unlocked");
@@ -30,7 +35,7 @@ public class DoorLock : MonoBehaviour,ILock
     public void DoorsOpen()
     {
         if (doorA != null)
-        {  
+        {
             doorA.transform.DOLocalMove(doorA_targetTransform.position, 1f);
         }
         if (doorB != null)
@@ -42,18 +47,18 @@ public class DoorLock : MonoBehaviour,ILock
     public void DoorsClose()
     {
         if (doorA != null)
-            doorA.transform.DOSmoothRewind();
+            doorA.transform.DOLocalMove(doorA_startPos, 1f);
         if (doorB != null)
-            doorB.transform.DOSmoothRewind();
+            doorB.transform.DOLocalMove(doorB_startPos, 1f);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(!foundKey) CheckForDoorKey();
-        if(!(other.gameObject.layer == 7 || other.gameObject.layer == 11))
+        if (!foundKey) CheckForDoorKey();
+        if (!(other.gameObject.layer == 7 || other.gameObject.layer == 11))
             return;
-        if(isUnlocked)
-        DoorsOpen();
+        if (isUnlocked)
+            DoorsOpen();
     }
 
     private void OnTriggerExit(Collider other)
@@ -66,9 +71,9 @@ public class DoorLock : MonoBehaviour,ILock
 
     void CheckForDoorKey()
     {
-        foreach(var key in PlayerKeyManager.instance.doorLockKeys)
+        foreach (var key in PlayerKeyManager.instance.doorLockKeys)
         {
-            if(key.GetKeyType()==type)
+            if (key.GetKeyType() == type)
             {
                 foundKey = true;
                 GetUnlocked();
